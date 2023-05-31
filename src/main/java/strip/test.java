066d1f576686cbb1da664fa190f19ab6822fe437
ramdanisalah67 <ramdanisalah67@gmail.com>
2023-05-31 17:42:01 +0200

    strip part 2 add card

--- src/main/java/strip/test.java
+++ src/main/java/strip/test.java
@@ -8,10 +8,11 @@ import com.google.gson.GsonBuilder;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
+import com.stripe.model.Token;
 
 public class test {
 
-	public static void main(String[] args) throws StripeException {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to strip");
 		Stripe.apiKey ="sk_test_51NDphaKqgASWsXIBpqkT7lQEYpO88IjzPMtkqyuFIKb351lbVckUU6dG17VirbEe25sPvpivp9QWuZOnTqA0GZkb008QiXaVBM";
@@ -21,11 +22,28 @@ public class test {
 		 Customer c = Customer.create(customerParameter) ;
 		 */
 		//retrieve a customer
-		 Customer c = Customer.retrieve("cus_NzpQpNKnBzYlsb") ;
+		 Customer c = Customer.retrieve("cus_NzqnpphJWka9ES") ;
 		// System.out.println(c);
 		 //convert result to json format
+		 
+		 //add card to customer
+	/*	 Map <String,Object> customerCard =new HashMap<String, Object>();
+		 customerCard.put("number", "4242424242424242");
+		 customerCard.put("exp_month", "11");
+		 customerCard.put("exp_year", "2023");
+		 customerCard.put("cvc", "123");
+		 Map <String,Object> tokenParam =new HashMap<String, Object>();
+		 tokenParam.put("card", customerCard);
+		 	
+		Token token = Token.create(tokenParam);
+			
+		Map <String,Object> source =new HashMap<String, Object>();
+		 source.put("source", token.getId());
+		 c.create(source)	;*/
+		 
 		 Gson json = new GsonBuilder().setPrettyPrinting().create();
 		 System.out.println(json.toJson(c));
-	}
+		 
+	}	
 
 }
